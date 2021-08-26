ba21376fd78d06e3b7c5021b087147f44bf7d730
Deepa-Tambad <86017554+Deepa-Tambad@users.noreply.github.com>
2021-08-26 19:06:17 +0530

    Update FileReaderCSV.scala

--- src/main/scala/util/FileReaderCSV.scala
+++ src/main/scala/util/FileReaderCSV.scala
@@ -9,41 +9,42 @@ import scala.util.{Failure, Success, Try}
 
 class FileReaderCSV extends App{
 
+  def convertCsvToTrainers(file: List[String]): Trainers = {
 
-def readCSVFile(path: String): Option[util.List[String]] = {
-    val data= Source.fromFile(path)
-    Try {
-          val list:Iterator[String]=data.getLines()
-          val data_List:util.List[String]=
-      case Success(source: Source) => {
-        return source.next.toString()
-      }
-      case Failure(exception) => {
-        return "Couldn't read the message" +exception.getMessage
+    val first: List[String] = file.head.split(",").toList
+
+    @tailrec
+    def readData(tail: List[String], front: List[String],
+                 trainers: List[Trainer] ): List[Trainer] = tail match {
+      case Nil => trainers
+      case head :: tail => {
+        val row_Value: List[String] = head.split(",").toList
+        val tuple: List[(String, String)] = front.zip(row_Value)
+        val tupleMap: Map[String, String] = tuple.toMap
+        readData(tail, front, trainers)
       }
     }
-  }
-  implicit val TrainerJsonFormat: RootJsonFormat[Trainer] = jsonFormat4(Trainer)
 
-  implicit val TrainersJsonFormat: RootJsonFormat[Trainers] = jsonFormat1(Trainers)
+    val trainers = List.empty[Trainer]
 
-  def convertToTrainer(trainer : String) = {
-    trainer.parseJson.convertTo[Trainer]
+    val trainer = Trainers.apply(readData(file.tail, first, tainers))
+    trainer
   }
-  def convertToTrainers(trainers : String ) = {
-    trainers.parseJson.convertTo[Trainers]
-  }
-  def readJsonFile(jsonSourcePath : String): List[String] = {
-    Try(Source.fromFile(jsonSourcePath)) match {
-      case Success(source: Source) => {
-        val file = source.getLines().toList
-        source.close()
-        file
-      }
-      case Failure(exception) => {
-        List.empty
-      }
-    }
+  def CsvToJson(file: List[String]): Trainers = {
+
+    val row = file.tail
+
+    val trainers: List[Trainer] = row.map { train =>
+      train.split(",")
+    }.map(train => Try(Some.apply(train.apply(0), train.apply(1), train.apply(2), train.apply(3))) match {
+      case Success(value) => value
+      case Failure(exception) =>
+        Some(train.apply(0), train.apply(1), train.apply(2),"")
+    }).map(train => {
+      val data: (String, String, String, String) = train.get
+      val mobile = if (data._4 != null && data._4.nonEmpty) Some.apply(data._4.toLong) else None
+      Trainer.apply(data._1.toLong, data._2, data._3, mobile)
+    })
+    Trainers.apply(trainers)
   }
 }
-
