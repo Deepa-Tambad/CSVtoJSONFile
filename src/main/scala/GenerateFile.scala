6da609d2ed05886a8181f7d1ffbd8741162f83dc
Deepa-Tambad <86017554+Deepa-Tambad@users.noreply.github.com>
2021-08-26 18:42:04 +0530

    Update GenerateFile.scala

--- src/main/scala/GenerateFile.scala
+++ src/main/scala/GenerateFile.scala
@@ -4,24 +4,9 @@ import util.{FileWriterJSON,FileReaderCSV}
 import scala.util.{Failure, Success, Try}
 object GenerateFile extends App {
 
-  private def functionCsvToJson = {
-    val fileContent: List[String] = Try(FileReader.readJsonFile(ApplicationConfig.config.getString("jsonpath"))) match {
-      case Success(value) => value
-      case Failure(exception) => List.empty
-    }
-
-    if (fileContent.isEmpty) {
-      ResultFileWriter
-        .logErrorToFile(ApplicationConfig.config.getString("error_path"),
-          "Couldn't read the Json File")
-    } else {
-      FileWriterJSON.convertMembersToJsonString(FileWriterJSON.convertCsvToMembers(fileContent),
-        ApplicationConfig.config.getString("csv_path"))
-    }
-  }
 
 
-  private def functionCsvToJson = {
+  private def CsvFileToJsonFile = {
     val file: List[String] = Try(FileReaderCSV.readJsonFile(ApplicationConfig.config.getString("jsonpath"))) match {
       case Success(value) => value
       case Failure(exception) => List.empty
@@ -30,10 +15,9 @@ object GenerateFile extends App {
     if (file.isEmpty) {
       FileWriterJSON.logErrorToFile(ApplicationConfig.config.getString("error_path"), "Couldn't read the CSV File")
     } else {
-      FileWriterJSON.convertMembersToJsonString(FileFileWriterJSON.convertCsvToTrainers(file),
+      FileWriterJSON.convertMembersToJsonString(FileWriterJSON.convertCsvToTrainers(file),
         ApplicationConfig.config.getString("csv_path"))
     }
   }
-
 }
 
